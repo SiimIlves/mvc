9f51dfd26796178802d8f7ba64052c5886eb8259
Siim Ilves <siim.ilves.001@khk.ee>
2020-09-09 09:52:21 +0300

    on lisatud main funktsioon programmi käivitamiseks

--- helpers.py
+++ helpers.py
@@ -1,29 +1,41 @@
 elemendid = []
 
-# lisame elemendi juurde
+# lisame ELEMENT juurde
 def lisa_element(nimetus, hind, kogus):
     global elemendid
-    elemendid.append({"nimetus":nimetus, "hind":hind, "kogus":kogus})
+    nimetused = []
+    for element in elemendid:
+        if nimetus in element.values():
+            nimetused.append(nimetus)
+    if nimetus in nimetused:
+        print("Element {} on juba olemas".format(nimetus))
+    else:
+        elemendid.append({"nimetus":nimetus, "hind":hind, "kogus":kogus})
 
 # lisame ELEMENDID KORRAGA juurde
 def lisa_elemendid(elementide_nimekiri):
+    global elemendid
     elemendid = elementide_nimekiri
 
 def main():
     # loome katseandmestik
     katse_elemendid = [
-        {"nimetus": "leib", "hind": 0.80, "kogus": 20},
-        {"nimetus": "piim", "hind": 0.50, "kogus": 15},
-        {"nimetus": "viin", "hind": 5.60, "kogus": 5},
+        {"nimetus": "leib", "hind":0.80, "kogus": 20},
+        {"nimetus": "piim", "hind":0.50, "kogus": 15},
+        {"nimetus": "vein", "hind":5.60, "kogus": 5},
     ]
+
     # testime elementide lisamist
-    lisa_elemendid()
+    lisa_elemendid(katse_elemendid)
     print(elemendid)
 
     # testime üksiku elemendi lisamist
     lisa_element("kohupiim", 0.90, 15)
     print(elemendid)
 
-# käivitamine
-if __name__ == "__name__":
+    lisa_element("vein", 5.60, 5)
+    print(elemendid)
+
+# käivitame
+if __name__ == "__main__":
     main()
