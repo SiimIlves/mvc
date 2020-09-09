a5d6dca195c2f8f685d6b9e8bb5bfe7104541c44
Siim Ilves <siim.ilves.001@khk.ee>
2020-09-09 11:36:27 +0300

    on lisatud kõikide elementide lugemine

--- helpers.py
+++ helpers.py
@@ -25,8 +25,46 @@ def loe_elemendid():
         loetud_elemendid.append(element)
     return loetud_elemendid
 
+# loeme KONKREETNE element
+def loe_element(nimetus):
+    global elemendid
+    nimetused = []
+    for element in elemendid:
+            nimetused.append(list(element.values())[0])
+    if nimetus not in nimetused:
+        return "Elementi {} ei eksisteeri".format(nimetus)
+    else:
+       return elemendid[nimetused.index(nimetus)]
+
+# uuendame KONKREETSET elementi
+def uuenda_element(nimetus, hind, kogus):
+    global elemendid
+    nimetused = []
+    for element in elemendid:
+        nimetused.append(list(element.values())[0])
+    if nimetus not in nimetused:
+        return "Elementi {} ei saa uuendada, kuna ta ei eksisteeri".format(nimetus)
+    else:
+        elemendid[nimetused.index(nimetus)] = {"nimetus":nimetus, "hind":hind, "kogus":kogus}
+
+# kustutame KONKREETSET elementi
+def kustuta_element(nimetus):
+    global elemendid
+    nimetused = []
+    for element in elemendid:
+        nimetused.append(list(element.values())[0])
+    if nimetus not in nimetused:
+        return "Elementi {} ei saa kustutada, kuna ta ei eksisteeri".format(nimetus)
+    else:
+        elemendid.remove(elemendid[nimetused.index(nimetus)])
+
+# kustutame KÕIK
+def kustuta_elemendid():
+    global elemendid
+    elemendid.clear()
+
 def main():
-    # loome katseandmestik
+    # loome katseandmestiku
     katse_elemendid = [
         {"nimetus": "leib", "hind":0.80, "kogus": 20},
         {"nimetus": "piim", "hind":0.50, "kogus": 15},
@@ -35,15 +73,27 @@ def main():
 
     # testime elementide lisamist
     lisa_elemendid(katse_elemendid)
-    print(elemendid)
 
     # testime üksiku elemendi lisamist
     lisa_element("kohupiim", 0.90, 15)
     lisa_element("viin", 5.60, 5)
 
     # testime elementide lugemist
+    print(loe_element("viin"))
+    print(loe_element("limonaad"))
+
+    #testime elemendi uuendamist
+    uuenda_element("viin", 10.0, 10)
+    print(loe_element("viin"))
+
+    # testime elemendi uuendamist
+    kustuta_element("viin")
+    print(loe_element("viin"))
+
+    # testime kõige kustutamist
+    kustuta_elemendid()
     print(loe_elemendid())
 
-# käivitamine
+# käivitame
 if __name__ == "__main__":
     main()
--- mvc_ui.py
+++ mvc_ui.py
@@ -1,5 +1,11 @@
+from helpers import lisa_element
+from model import Model
+
 elemendid = [
-    {"nimetus": "leib", "hind": 0.80, "kogus": 20}
-    {"nimetus": "piim", "hind": 0.50, "kogus": 15}
-    {"nimetus": "viin", "hind": 5.60, "kogus": 5}
+    {"nimetus": "leib", "hind": 0.80, "kogus": 20},
+    {"nimetus": "piim", "hind": 0.50, "kogus": 15},
+    {"nimetus": "viin", "hind": 5.60, "kogus": 5},
 ]
+
+pood = Model(elemendid)
+print(pood.loe_elemendid())
