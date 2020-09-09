81827434c343867db1781f9cc17f5fa69ea00c18
Siim Ilves <siim.ilves.001@khk.ee>
2020-09-09 09:10:02 +0300

    on lisatud main funktsioon programmi käivitamiseks

--- helpers.py
+++ helpers.py
@@ -9,16 +9,21 @@ def lisa_element(nimetus, hind, kogus):
 def lisa_elemendid(elementide_nimekiri):
     elemendid = elementide_nimekiri
 
-# loome katseandmestik
-katse_elemendid = [
-    {"nimetus": "leib", "hind": 0.80, "kogus": 20},
-    {"nimetus": "piim", "hind": 0.50, "kogus": 15},
-    {"nimetus": "viin", "hind": 5.60, "kogus": 5},
-]
-# testime elementide lisamist
-lisa_elemendid()
-print(elemendid)
+def main():
+    # loome katseandmestik
+    katse_elemendid = [
+        {"nimetus": "leib", "hind": 0.80, "kogus": 20},
+        {"nimetus": "piim", "hind": 0.50, "kogus": 15},
+        {"nimetus": "viin", "hind": 5.60, "kogus": 5},
+    ]
+    # testime elementide lisamist
+    lisa_elemendid()
+    print(elemendid)
 
-# testime üksiku elemendi lisamist
-lisa_element("kohupiim", 0.90, 15)
-print(elemendid)
+    # testime üksiku elemendi lisamist
+    lisa_element("kohupiim", 0.90, 15)
+    print(elemendid)
+
+# käivitamine
+if __name__ == "__name__":
+    main()
